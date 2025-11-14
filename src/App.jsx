ba82144a1f156889f51a80eb3e0224060a215bb1
Rahma <rahmaputriazzahra@gmail.com>
2025-11-14 21:06:29 +0700

    feat(app): arrange item in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css";
 import Button from "./components/Button";
 import CheckboxItem from "./components/CheckboxItem";
+import Divider from "./components/Divider";
 
 function App() {
   return (
     <div className="backdrop">
-      <Button text="Done" />
       <div className="container">
         <CheckboxItem />
+        <Divider />
+        {Array.from({ length: 4 }).map((_, index) => (
+          <CheckboxItem key={index} label={`Page ${index + 1}`} />
+        ))}
+        <Divider />
+        <div className="button-container">
+          <Button text="Done" />
+        </div>
       </div>
     </div>
   );
