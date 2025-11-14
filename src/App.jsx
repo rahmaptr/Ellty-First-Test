1e8e9663c8aa24fd3c9b4dbaa2873f7c8149de66
Rahma <rahmaputriazzahra@gmail.com>
2025-11-14 20:09:22 +0700

    feat(app): arrange items

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import './App.css'
-import Button from './components/Button'
-import CheckboxItem from './components/CheckboxItem'
+import "./App.css";
+import Button from "./components/Button";
+import CheckboxItem from "./components/CheckboxItem";
 
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <Button text='Done'/>
-      <CheckboxItem />
-    </>
-  )
+    <div className="backdrop">
+      <Button text="Done" />
+      <div className="container">
+        <CheckboxItem />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import './Button.css';
 
 export default function Button(item) {
-  return <button className='container'>{item.text}</button>;
+  return <button className='button'>{item.text}</button>;
 }
