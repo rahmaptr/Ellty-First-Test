a44c2b87c4a08b940144bcb405acad96436b1d78
Rahma <rahmaputriazzahra@gmail.com>
2025-11-14 19:57:01 +0700

    style(checkbox): add transition style in different state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Button from './components/Button'
+import CheckBox from './components/CheckBox'
 
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
       <Button text='Done'/>
+      <CheckBox />
     </>
   )
 }
--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,10 +1,26 @@
-import './CheckBox.css';
-import CheckIcon from './icons/CheckIcon';
+import { useState } from "react";
+import "./CheckBox.css";
+import CheckIcon from "./icons/CheckIcon";
 
-export default function CheckBox(item) {
+export default function CheckBox() {
+  const [isChecked, setIsChecked] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+   const getIconColor = () => {
+    if (isChecked) {
+      return isHovered ? "#E3E3E3" : "#FFFFFF";
+    }
+    return "#878787";
+  };
+
   return (
-    <div className='container'>
-      <CheckIcon color='#CDCDCD'/>
+    <div
+      className={`checkbox ${isChecked ? "checked" : ""}`}
+      onClick={() => setIsChecked(!isChecked)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {isChecked && <CheckIcon color={getIconColor()} id="check-icon" />}
     </div>
   );
 }
